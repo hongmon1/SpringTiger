ea52e95d08f1852f0029102eb1806c0540f3b336
hongmon1 <39690330+hongmon1@users.noreply.github.com>
2019-08-08 23:14:45 +0900

    패키지 정리

--- app/src/main/java/com/example/springtigersapp/View/MainActivity.java
+++ app/src/main/java/com/example/springtigersapp/View/MainActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.springtigersapp.R;
+
 public class MainActivity extends AppCompatActivity {
 
     //안녕예주야
-
+    //안녕소현아
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
