c0d5b5b10824f33d38ad1d13e3894c54dc2064a7
hongmon1 <39690330+hongmon1@users.noreply.github.com>
2019-08-14 20:46:50 +0900

    HONG test commit

---
+++ app/src/main/java/com/example/springtigersapp/View/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.springtigersapp.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.springtigersapp.R;
+
+public class SearchActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
