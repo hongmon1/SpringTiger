0c6471edc1470bdcc7bbb24d636498e221db6883
hongmon1 <39690330+hongmon1@users.noreply.github.com>
2019-08-28 20:12:55 +0900

    DB 폴더 생성

---
+++ app/src/main/java/com/example/springtigersapp/DB/DataBases.java
@@ -0,0 +1,12 @@
+package com.example.springtigersapp.DB;
+
+import android.provider.BaseColumns;
+
+//Scheme 정의 클래스
+//BaseColumns 인터페이스 구현
+//final class - 상속 불가
+public final class DataBases {
+    public static final class CreateDB implements BaseColumns{
+
+    }
+}
