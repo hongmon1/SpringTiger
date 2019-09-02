6d6402a2d075762cf73546fe3b1453dccfdbd32a
beginner <kisonnn@naver.com>
2019-09-03 06:07:24 +0900

    logo수정완료(메니페스트수정)

--- app/src/main/java/com/example/springtigersapp/View/LogoActivity.java
+++ app/src/main/java/com/example/springtigersapp/View/LogoActivity.java
@@ -1,6 +1,18 @@
 package com.example.springtigersapp.View;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogoActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(LogoActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
--- app/src/main/java/com/example/springtigersapp/View/NaviActivity.java
+++ app/src/main/java/com/example/springtigersapp/View/NaviActivity.java
@@ -1,6 +1,35 @@
 package com.example.springtigersapp.View;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.springtigersapp.Model.Navi;
+import com.example.springtigersapp.R;
+
 public class NaviActivity extends AppCompatActivity {
+
+    Button button8;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi);
+
+
+
+
+        button8 = findViewById(R.id.button);
+        button8.setOnClickListener(new View.OnClickListener() {
+            Navi navi = new Navi(NaviActivity.this);
+            @Override
+            public void onClick(View view) {
+                navi.turnGPSOn();
+            }
+        });
+    }
+
 }
