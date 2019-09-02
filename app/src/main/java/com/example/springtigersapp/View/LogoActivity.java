171e496215311fd360c9ee48224b5630a868118b
hongmon1 <39690330+hongmon1@users.noreply.github.com>
2019-09-03 02:21:22 +0900

    font 추가
    font 폴더 생성
    font별로 family xml파일 생성

--- app/src/main/java/com/example/springtigersapp/View/LogoActivity.java
+++ app/src/main/java/com/example/springtigersapp/View/LogoActivity.java
@@ -1,6 +1,18 @@
 package com.example.springtigersapp.View;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class LogoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
 }
