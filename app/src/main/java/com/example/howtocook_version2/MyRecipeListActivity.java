4b84705a5d4ce8a9e9ae6b63d6ba30cc6bdd563e
YoumHyeJi <yheji0321@gmail.com>
2021-02-17 14:39:57 +0900

    db

---
+++ app/src/main/java/com/example/howtocook_version2/MyRecipeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.howtocook_version2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+class MyRecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recipe);
+
+    }
+}
