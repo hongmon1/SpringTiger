0153e9f2e36c747f6934e54602ede851600566bc
hongmon1 <hongmon1@naver.com>
2021-02-17 21:59:36 +0900

    recipeview xml 수정

--- app/src/main/java/com/example/howtocook_version2/MyRecipeActivity.java
+++ app/src/main/java/com/example/howtocook_version2/MyRecipeActivity.java
@@ -2,15 +2,48 @@ package com.example.howtocook_version2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MyRecipeActivity extends AppCompatActivity {
+
+    private DBOpenHelper mDbOpenHelper;
+    private ListViewItem item;
+
+    private ImageView imageView;
+    private EditText register_name, register_ingre, register_desc;
+
+    private String C_name;
+
+    private Cursor mCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_view);
 
+        getSupportActionBar().hide();
 
+        Intent intent = getIntent();
+
+        C_name = intent.getExtras().getString("name");
+
+        mDbOpenHelper = new DBOpenHelper(this);
+        mDbOpenHelper.open();
+
+        mCursor = null;
+        mCursor = mDbOpenHelper.getMyrepAllColumns();
+
+        while(mCursor.moveToNext()){
+            if(mCursor.getString(mCursor.getColumnIndex("name")).equals(C_name)){
+
+
+            }
+        }
     }
 }
