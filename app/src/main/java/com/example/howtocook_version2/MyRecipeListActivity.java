4f5be82abd7bcef197dbaa8c664de4b456d9275b
YoumHyeJi <yheji0321@gmail.com>
2021-02-17 14:57:47 +0900

    db

--- app/src/main/java/com/example/howtocook_version2/MyRecipeListActivity.java
+++ app/src/main/java/com/example/howtocook_version2/MyRecipeListActivity.java
@@ -1,14 +1,23 @@
 package com.example.howtocook_version2;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 class MyRecipeListActivity extends AppCompatActivity {
 
+    ListView listview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_recipe);
+        setContentView(R.layout.avtivity_my_recipe_list);
+
+
 
     }
 }
