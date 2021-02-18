388b0440d60c46e152030e20ebc0cda066319127
hongmon1 <hongmon1@naver.com>
2021-02-18 16:52:37 +0900

    manifest수정, xml 수정

--- app/src/main/java/com/example/howtocook_version2/ListViewAdapter.java
+++ app/src/main/java/com/example/howtocook_version2/ListViewAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -44,6 +45,7 @@ public class ListViewAdapter extends BaseAdapter {
 
         //화면에 표시될 View로부터 위젯에 대한 참조 획득
         ImageView imageView = convertView.findViewById(R.id.cook_img);
+
         TextView textView = convertView.findViewById(R.id.cook_name);
 
         //data set에서 position에 위치한 데이터 참조 획득
